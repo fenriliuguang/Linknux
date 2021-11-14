565fd8c6d388a99b9d225e03ad8962746e1e3d32
LRlicious <1072120846@qq.com>
2021-11-14 12:04:23 +0800

    新增历史记录
    新增历史记录

--- pages/user/user.js
+++ pages/user/user.js
@@ -5,7 +5,26 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        history_page:[
+            {
+                id:0,
+                text:"absdasdas",
+                date:"2000.1.1",
+                time:"14:20",
+            },
+            {
+                id:1,
+                text:"absasdwadasdasdas",
+                date:"2000.10.1",
+                time:"20:10",
+            },
+            {
+                id:2,
+                text:"adasdasdzsdas",
+                date:"2020.11.11",
+                time:"7:01",
+            },
+        ]
     },
 
     /**
